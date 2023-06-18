f9aae6b1fdc155babc19da66d6198b8a221dfbb3
Maninder <manindersingh29650@gmail.com>
2023-06-18 15:29:14 +0530

    work

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const MONGO_URI = 'mongodb+srv://maninder:maninder@cluster0.iw394ij.mongodb.net/?retryWrites=true'
+const MONGO_URI = 'mongodb+srv://maninder:maninder@cluster0.iw394ij.mongodb.net/?retryWrites=true&w=majority'
 exports.connectDatabase = ()=>{
     mongoose.connect(MONGO_URI).then(console.log("database connected")).catch((err)=>console.log(err));
     
